2862d399864307a3c716d89dcada121aae22f387
ĐỨC TRUNG <duct5732@gmail.com>
2024-06-11 22:57:56 +0700

    g

--- api/index.js
+++ api/index.js
@@ -24,19 +24,10 @@ const app = express()
 const httpServer = createServer(app);
 
 
-const io = new Server(httpServer, {
-    cors: {
-        origin: 'http://localhost:5173',
-        methods: ["GET", "POST"]
-    }
-});
+const io = new Server(httpServer);
 
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}))
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 
