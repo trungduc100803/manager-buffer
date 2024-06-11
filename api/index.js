c3ff0373459cc81f98c6e8e61628a16e3d09507b
ĐỨC TRUNG <duct5732@gmail.com>
2024-06-11 22:35:43 +0700

    e

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
         origin: 'http://localhost:5173',
-        methods: ["GET", "POST", "DELETE", "PUT"],
+        methods: ["GET", "POST"]
     }
 });
 
