e5eb3d7d1597ff33e1bc9ee70c7db114ac060b25
ĐỨC TRUNG <duct5732@gmail.com>
2024-06-11 23:02:22 +0700

    h

--- api/index.js
+++ api/index.js
@@ -80,11 +80,6 @@ const __dirname = path.resolve()
 
 app.use(express.static(path.join(__dirname, '/client/dist')))
 app.get('*', (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
 })
 
